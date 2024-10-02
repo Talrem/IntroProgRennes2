4b001d70037f7ca1a7774be11ede78759bb5f75a
Talrem <43709968+Talrem@users.noreply.github.com>
2024-10-02 17:34:35 +0200

    Fix Cypher

--- scripts/cesarCypher.js
+++ scripts/cesarCypher.js
@@ -1,45 +1,55 @@
 function getAlphaNumber(a){
- switch(a){
-  case "a":return 1;
-  case "b":return 2;
-  case "c":return 3;
-  case "d":return 4;
-  case "e":return 5;
-  case "f":return 6;
-  case "g":return 7;
-  case "h":return 8;
-  case "i":return 9;
-  case "j":return 10;
-  case "k":return 11;
-  case "l":return 12;
-  case "m":return 13;
-  case "n":return 14;
-  case "o":return 15;
-  case "p":return 16;
-  case "q":return 17;
-  case "r":return 18;
-  case "s":return 19;
-  case "t":return 20;
-  case "u":return 21;
-  case "v":return 22;
-  case "w":return 23;
-  case "x":return 24;
-  case "y":return 25;
-  case "z":return 26;
-  default: return 0;
- }
+  switch(a){
+    case "a":return 0;
+    case "b":return 1;
+    case "c":return 2;
+    case "d":return 3;
+    case "e":return 4;
+    case "f":return 5;
+    case "g":return 6;
+    case "h":return 7;
+    case "i":return 8;
+    case "j":return 9;
+    case "k":return 10;
+    case "l":return 11;
+    case "m":return 12;
+    case "n":return 13;
+    case "o":return 14;
+    case "p":return 15;
+    case "q":return 16;
+    case "r":return 17;
+    case "s":return 18;
+    case "t":return 19;
+    case "u":return 20;
+    case "v":return 21;
+    case "w":return 22;
+    case "x":return 23;
+    case "y":return 24;
+    case "z":return 25;
+    default: return -1; // si le charactère n'est pas reconnu, on renvoit le code de l'espace. Le traitement se fait dans l'autre fonction
+  }
 }
 
 function cesarCypher(){
- var message = prompt("Quel est votre message ? ");
- var messageCode = "";
- var key = parseInt(prompt("Quelle est votre clé de déplacement ? "));
- var alphabet = [" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
- var messageArray = Array.from(message);
- console.log(messageArray);
- for(i = 0; i < messageArray.length; i++){
-   console.log(getAlphaNumber(messageArray[i]));
-   messageCode += alphabet[(getAlphaNumber(messageArray[i]) + key)];
- }
- alert(messageCode);
+  var message = prompt("Quel est votre message ? ").toLowerCase();
+  var messageCode = "";
+  var key = parseInt(prompt("Quelle est votre clé de déplacement ? "));
+  var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var messageArray = Array.from(message);
+  var cNumber = 0;
+  for(i = 0; i < messageArray.length; i++){
+    if(getAlphaNumber(messageArray[i])!=-1){
+      cNumber = getAlphaNumber(messageArray[i]) + key;
+      //On sort du tableau par en dessous.
+      if(cNumber < 0){
+        //Donc on doit remonter
+        cNumber += 26;
+      }
+      messageCode += alphabet[(cNumber%26)]; //l'opérateur % (modulo) permet d'avoir le reste d'une division euclidienne.
+    }else{
+      //Le charactère n'a pas été reconnu, donc on remet le même.
+      messageCode += messageArray[i];
+    }
+  }
+  alert(messageCode);
 }
