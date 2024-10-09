5a35aaf5c3c2476ce6af200bc60885adcf2ea342
Talrem <43709968+Talrem@users.noreply.github.com>
2024-10-09 16:14:57 +0200

    Prog 5

--- scripts/racineVerbale.js
+++ scripts/racineVerbale.js
@@ -2,7 +2,9 @@ function racineVerbale(){
   var mot = prompt("Donnez moi un verbe du premier groupe : ");
   var racine = "";
   //on utilise le for pour parcourir le mot lettre par lettre comme un tableau
+  //Puisqu'on veut retirer le "er", on va aller jusqu'à length-2 dans le parcours du mot
   for(var i = 0; i < mot.length-2 ; i++){
+    //On adjoint à la racine la lettre actuelle du mot
     racine+=mot[i];
   }
   racine+="-";
